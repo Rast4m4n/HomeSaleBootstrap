ec3ceb35a2cc45ad450c5c3e45238a5db5dc3603
Kirill <motovilink@yandex.ru>
2022-12-01 22:15:08 +0300

    Добавил рандомные картинки, названия, цену и тд. для карточки недвижимости. Исправил название картинок. Теперь на страницу просмотра недвижимочти передаются аргументы c данными

--- homeAndArea/home_and_area.js
+++ homeAndArea/home_and_area.js
@@ -1,11 +1,59 @@
 const body = document.getElementById("realtyView");
+const path = ["area/area", "house/houseH"];
+const nameRealty = ["Небоскрёб",
+    "Саншайново",
+    "Миднайт",
+    "Мот",
+    "Трактирщик",
+    "Утренняя звезда",
+    "Мораль",
+    "Благ",
+    "Эдем",
+    "Пандора",
+    "Каскад",
+    "Мир",
+    "Лояльность",
+    "Мироздание",
+    "Творчества",
+    "Колейдоскоп"];
+const develope = ["Санрайз",
+    "СкайСлайсер",
+    "Меломан",
+    "ПИК",
+    "Зелёный мир",
+    "Преисполнение",
+    "Флюид",
+    "Отладочная",
+    "Денница"];
+const address = ["Улица льнинова",
+"Улица сногшибательская",
+"Улица уморительного",
+"Улица структурная",
+"Улица кривоухова",
+"Улица трактова",
+"Улица гранёного",
+"Улица джейсон",
+"Улица меркантильного",
+"Улица нравственного",
+"Улица вальяжная",
+"Улица нормативности",
+"Улица круасаново"];
 
-function createRealty() {
+function generateRealty() {
+    const randRealty = path[Math.floor(Math.random() * path.length)];
+    const randImage = Math.floor(Math.random() * (9 - 1) + 1);
+    const randName = nameRealty[Math.floor(Math.random() * nameRealty.length)];
+    const randDev = develope[Math.floor(Math.random() * develope.length)]; 
+    const randPrice = `От ${Math.floor(Math.random() *20 +20)} млн. до ${Math.floor(Math.random()* 60 +40)} млн. руб.`;
+    const randAddress = address[Math.floor(Math.random() * address.length)];
     const realty = document.createElement('div');
-    realty.innerHTML = '<div class="col" style="padding: 12px;"><a href="../viewRealty/view_realty.html" class="link-dark" style="text-decoration: none;"><div class="card"><img src="../assets/images/house/houseH1.jpg" alt="" class="card-img-top"><div class="card-body"><div class="card-subtitle" style="font-weight: bold;">ЖК Мир</div><div class="card-text">От 20 млн. до 60 млн. руб.</div><div class="card-text">Улица дмитриевского</div><br></div><div class="card-footer "> Застройщик ПИК</div></div></a></div>';
+    console.log(randRealty);
+    realty.innerHTML = `<div class="col" style="padding: 12px;"><a href="../viewRealty/view_realty.html?image=${randImage}&type=${randRealty}&name=${randName}&develope=${randDev}&price=${randPrice}&address=${randAddress}" class="link-dark" style="text-decoration: none;"><div class="card" ><img src="../assets/images/${randRealty}${randImage}.jpg" alt="" class="card-img-top" id="cardWidget"><div class="card-body" ><div class="card-subtitle" style="font-weight: bold;">ЖК ${randName}</div><div class="card-text">${randPrice}</div><div class="card-text">${randAddress}</div><br></div><div class="card-footer "> Застройщик ${randDev}</div></div></a></div>`;
     return realty;
 }
 
+
 for (let i = 0; i < 100; i++) {
-    body.append(createRealty());
+    body.append(generateRealty());
 }
+
